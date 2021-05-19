1b4ebdb184411d209dbffab5a68bd9ba4a545f5e
Rasmus Lyxell <c19rll@cs.umu.se>
2021-05-19 16:52:08 +0200

    IDk

--- rem1/Fjärr 1/main.c
+++ rem1/Fjärr 1/main.c
@@ -29,16 +29,13 @@
 #define LEDG 5
 #define LEDR1 6
 #define LEDR2 7
-#define MAX_MSG_LEN 16
 #define FRONT_POS 5
 #define BACK_POS 20
 #define CL_POS 30
 #define CR_POS 31
 #define PIR_POS 15
-#define MSGBUF_SIZE 8
-#define MSGBUF_BITMASK MSGBUF_SIZE-1
+#define CMD_DELAY 20
 
-#define mask_circ_ind(x) ((x) & (MSGBUF_BITMASK))
 #define SETBIT(x, y) ((x) |= (1<<(y)))		//Sets bit y on port x
 #define CLEARBIT(x, y) ((x) &= ~(1<<(y)))	//Clears bit y on port x
 
@@ -58,6 +55,7 @@ static FILE lcdstdout = FDEV_SETUP_STREAM(lcd_put_char, NULL, 0);
 volatile uint8_t uart_linebuf[20];
 volatile uint8_t uart_bufind = 0;
 volatile uint8_t heart_hundreths = 0;
+volatile uint8_t command_hundreths = 0;
 volatile char sensor_buf[16];
 volatile bool sensors_received = false;
 volatile uint16_t x_rest = 511;
@@ -96,34 +94,36 @@ int main(void)
 				uart_send_line("041");
 				LED_PORT |= 1<<LEDG;
 			}
-			int16_t adjusted_x = x_rest - read_avg_adc(1, 25);
-			int16_t adjusted_y = y_rest - read_avg_adc(2, 25);
-			int16_t left = adjusted_x-adjusted_y;
-			int16_t right = adjusted_x+adjusted_y;
-			if(left > 127){
-				left = 127;
+			if(command_hundreths == CMD_DELAY){
+				command_hundreths = 0;
+				int16_t adjusted_x = x_rest - read_avg_adc(1, 25);
+				int16_t adjusted_y = y_rest - read_avg_adc(2, 25);
+				int16_t left = adjusted_x-adjusted_y;
+				int16_t right = adjusted_x+adjusted_y;
+				if(left > 127){
+					left = 127;
+				}
+				else if(left < -127){
+					left = -127;
+				} else if(left > -10 && left < 10) left = 0;
+				
+				if(right > 127) {
+					right = 127;
+				}
+				else if(right < -127){
+					right = -127;
+				} else if(right > -10 && right < 10) right = 0;
+				
+				if(left != last_left || right != last_right){
+					itoa(left, itoabuf+2, 10);
+					uint8_t len = strlen(itoabuf);
+					itoabuf[len++] = ' ';
+					itoa(right, itoabuf+len, 10);
+					uart_send_line(itoabuf);
+				}
+				last_left = left;
+				last_right = right;
 			}
-			else if(left < -127){
-				left = -127;
-			} else if(left > -10 && left < 10) left = 0;
-			
-			if(right > 127) {
-				right = 127;
-			}
-			else if(right < -127){
-				right = -127;
-			} else if(right > -10 && right < 10) right = 0;
-			
-			if(left != last_left || right != last_right){
-				itoa(left, itoabuf+2, 10);
-				uint8_t len = strlen(itoabuf);
-				itoabuf[len++] = ' ';
-				itoabuf[len] = '\0';
-				itoa(right, itoabuf+len, 10);
-				uart_send_line(itoabuf);
-			}
-			last_left = left;
-			last_right = right;
 		} else{
 			if(LED_PORT & (1<<LEDG)){
 				uart_send_line("040");
@@ -155,6 +155,9 @@ ISR(TIMER2_COMPA_vect){
 	if(++heart_hundreths > 50){
 		LED_PORT |= 1<<LEDR2;
 		heart_hundreths = 0;
+	}
+	if(command_hundreths < CMD_DELAY){
+		command_hundreths++;
 	}
 	messages_timerproc();
 }
@@ -195,20 +198,33 @@ void perform_command(uint8_t topic, uint8_t command, volatile uint8_t* args){
 					queue_message((char*)args);
 					break;
 				case '2':
-					Summer_PlayMelody(MELODY_HONK);
+					Summer_PlayMelody(args[0]-'0');
 					break;
 			}
 			break;
 	}
 }
 
-bool button_is_pressed(char* args){
-	return args[strlen(args)-2] == '0';
-}
-
 void display_sensors(){
 	char* args = (char*)sensor_buf;
-	uint8_t b = 0;
+	
+	set_cursor_pos(CR_POS);
+	write_lcd_char((args[0] == '0') ? 'R' : '_');
+	set_cursor_pos(CL_POS);
+	write_lcd_char((args[1] == '0') ? 'L' : '_');
+	set_cursor_pos(PIR_POS);
+	write_lcd_char((args[3] == '0') ? 'X' : '_');
+	if(args[2] == '0'){
+		if(!(LED_PORT & 1<<LEDR1)){
+			Summer_PlayMelody(MELODY_HONK);
+			LED_PORT |= 1<<LEDR1;
+		}
+		} else{
+		LED_PORT &= ~(1<<LEDR1);
+	}
+	
+	
+	uint8_t b = 5;
 	while(args[++b] != ' ');
 	args[b++] = '\0';
 	uint8_t k = b;
@@ -217,11 +233,11 @@ void display_sensors(){
 	
 	
 	set_cursor_pos(FRONT_POS);
-	if(b == 2 && args[0] == '0'){ //0 is sent for front
+	if(b == 7 && args[0] == '0'){ //0 is sent for front
 		write_lcd_string("!!!");
 	} else{
-		for (int8_t i = 3-strlen(args); i > 0; i--) write_lcd_char(' ');
-		write_lcd_string(args);
+		for (int8_t i = 3-strlen(args+5); i > 0; i--) write_lcd_char(' ');
+		write_lcd_string(args+5);
 	}
 	
 	set_cursor_pos(BACK_POS);
@@ -230,20 +246,6 @@ void display_sensors(){
 	} else{
 		for (int8_t i = 3-strlen(args+b); i > 0; i--) write_lcd_char(' ');
 		write_lcd_string(args+b);
-	}
-	set_cursor_pos(CR_POS);
-	write_lcd_char((args[k] == '0') ? 'R' : '_');
-	set_cursor_pos(CL_POS);
-	write_lcd_char((args[k+1] == '0') ? 'L' : '_');
-	set_cursor_pos(PIR_POS);
-	write_lcd_char((args[k+3] == '0') ? 'X' : '_');
-	if(args[k+2] == '0'){
-		if(!(LED_PORT & 1<<LEDR1)){
-			//Summer_PlayMelody(MELODY_HONK);
-			LED_PORT |= 1<<LEDR1;
-		}
-	} else{
-		LED_PORT &= ~(1<<LEDR1);
 	}
 }
 
