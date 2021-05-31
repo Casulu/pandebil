92667d7145e0968eccc2406f0cf9432e79619c31
Rasmus Lyxell <rasmuslyxell@gmail.com>
2021-05-31 12:55:36 +0200

    Tar bort newline

--- rem1_mqtt.py
+++ rem1_mqtt.py
@@ -1,7 +1,7 @@
 import serial
 import paho.mqtt.client as mqtt
 
-serialport = 'COM1'
+serialport = 'COM6'
 baudrate = 500000
 broker = "tfe.iotwan.se"
 brokerport = 1883
@@ -47,7 +47,7 @@ try:
         
     while True:
         try:
-            line = ser.readline().decode()
+            line = ser.readline().decode().strip()
         except:
             print("Could not decode incoming line")
             continue
